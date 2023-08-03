b3ad5850afa2d23ec9641efdc36cf791a54a8a29
marvell17 <marvellpereira.52@gmail.com>
2023-08-03 18:00:45 -0400

    Create calculator.py main code

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        try:
+            print("Result:", divide(num1, num2))
+        except ValueError as e:
+            print("Error:", str(e))
+    else:
+        print("Invalid input. Please select a valid operation (1/2/3/4).")
+
+if __name__ == "__main__":
+    calculator()
